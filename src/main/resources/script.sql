ec401b2e52eedf5f5839cb13183e348bb1af96b0
Dee <dee@zipcodes-MacBook-Pro-2.local>
2021-08-10 20:22:43 -0400

    dependency and script added

---
+++ src/main/resources/script.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO movies (title, runtime, genre, imdb_score, rating) VALUES ('Howard the Duck', 110, 'Sci-Fi', 4.6, 'PG');
+INSERT INTO movies (title, runtime, genre, imdb_score, rating) VALUES ('Lavalantula', 83, 'Horror', 4.7, 'TV-14');
+INSERT INTO movies (title, runtime, genre, imdb_score, rating) VALUES ('Starship Troopers', 129, 'Sci-Fi', 7.2, 'PG-13');
+INSERT INTO movies (title, runtime, genre, imdb_score, rating) VALUES ('Waltz With Bashir', 90, 'Documentary', 8.0, 'R');
+INSERT INTO movies (title, runtime, genre, imdb_score, rating) VALUES ('Spaceballs', 96, 'Comedy', 7.1, 'PG');
+INSERT INTO movies (title, runtime, genre, imdb_score, rating) VALUES ('Monster Inc.', 92, 'Animation', 8.1, 'G');
+INSERT INTO movies (title, runtime, genre, imdb_score, rating) VALUES ('Suicide Squad', 97, 'Action', 5.9, 'PG-13');
+INSERT INTO movies (title, runtime, genre, imdb_score, rating) VALUES ('Get Out', 110, 'Horror', 8.9, 'R');
+INSERT INTO movies (title, runtime, genre, imdb_score, rating) VALUES ('Jump', 28, 'Family', 7.2, 'G');
